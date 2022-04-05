5bb61ad5e03fa4762ccd4371f92bdf9d4c6d1e3d
Carson <cpsievert1@gmail.com>
2022-04-05 10:26:37 -0500

    Close #26: schedule a Shiny.bindAll() to occur a tick after registering

--- js/src/output.ts
+++ js/src/output.ts
@@ -54,6 +54,14 @@ class IPyWidgetOutput extends Shiny.OutputBinding {
 
 Shiny.outputBindings.register(new IPyWidgetOutput(), "shiny.IPyWidgetOutput");
 
+// By the time this code executes (i.e., by the time the `callback` in
+// `require(["@jupyter-widgets/html-manager"], callback)` executes, it seems to be too
+// late to register the output binding in a way that it'll "just work" for dynamic UI.
+// Moreover, when this code executes, dynamic UI when yet be loaded, so schedule
+// a bind to happen on the next tick.
+// TODO: this is a hack. Could we do something better?
+setTimeout(() => { Shiny.bindAll(document.body); }, 0);
+
 
 /******************************************************************************
 * Handle messages from the server-side Widget
