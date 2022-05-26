37cc747a7d69e34f3fe5e6679a61e8d883fafc59
Winston Chang <winston@stdout.org>
2022-05-26 17:12:23 -0500

    Packaging updates (#41)

--- ipyshiny/__init__.py
+++ ipyshiny/__init__.py
@@ -1,7 +1,8 @@
 """Top-level package for ipyshiny."""
 
 __author__ = """Carson Sievert"""
-__email__ = 'carson@rstudio.com'
-__version__ = '0.1.0'
+__email__ = "carson@rstudio.com"
+__version__ = "0.1.0.9000"
+
 
 from ._ipyshiny import *
