6b60427b497fcb1b1c3515be8c67593371f9666c
Carson <cpsievert1@gmail.com>
2022-07-27 16:56:31 -0400

    Bump version

--- shinywidgets/__init__.py
+++ shinywidgets/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Carson Sievert"""
 __email__ = "carson@rstudio.com"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 
 from ._shinywidgets import output_widget, register_widget, render_widget, reactive_read
