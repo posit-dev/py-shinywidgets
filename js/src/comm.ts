0ef915929e6d1020ce0926594b019769264a17f1
Carson <cpsievert1@gmail.com>
2023-04-05 10:20:02 -0500

    Add `Throttler` class to be used in `ShinyComm` to throttle mousemove events

--- js/src/comm.ts
+++ js/src/comm.ts
@@ -1,3 +1,67 @@
+class Throttler {
+  fnToCall: Function;
+  wait: number;
+  timeoutId: ReturnType<typeof setTimeout>;
+  get isWaiting(): boolean {
+    return this.timeoutId !== null;
+  }
+  constructor(wait: number = 100) {
+    if (wait < 0) throw new Error("wait must be a positive number");
+    this.wait = wait;
+    this._reset();
+  }
+  _reset() {
+    this.fnToCall = null;
+    clearTimeout(this.timeoutId);
+    this.timeoutId = null;
+  }
+  _setTimeout() {
+    this.timeoutId = setTimeout(() => {
+      if (this.fnToCall) {
+        // console.log("Timeout!", Date.now());
+
+        // Call the function
+        this.fnToCall();
+        this.fnToCall = null;
+        // Restart the timeout as we just called the function
+        // This call is the key step of Throttler
+        this._setTimeout();
+      } else {
+        // console.log("Empty!", Date.now());
+
+        // Nothing was done, so reset
+        this._reset();
+      }
+    }, this.wait);
+  }
+  // Execute the function immediately and reset the timeout
+  // This is useful when the timeout is waiting and we want to
+  // execute the function immediately to not have events be out
+  // of order
+  flush() {
+    // console.log("Flush!", Date.now());
+
+    if (this.fnToCall) this.fnToCall();
+    this._reset();
+  }
+  // Try to execute the function immediately, if it is not waiting
+  // If it is waiting, update the function to be called
+  throttle(fn: Function) {
+    if (!this.isWaiting) {
+      // console.log("Call!", Date.now());
+
+      // If there is nothing waiting, call it immediately
+      // and start the throttling
+      fn();
+      this._setTimeout();
+    } else {
+      // If the timeout is currently waiting, update the func to be called
+      this.fnToCall = fn;
+    }
+  }
+
+}
+
 // This class is a striped down version of Comm from @jupyter-widgets/base
 // https://github.com/jupyter-widgets/ipywidgets/blob/88cec8/packages/base/src/services-shim.ts#L192-L335
 // Note that the Kernel.IComm implementation is located here
@@ -17,6 +81,8 @@ export class ShinyComm {
 
   _msg_callback;
   _close_callback;
+  // Throttle mousemove events to every 100ms
+  _mouse_move_throttler = new Throttler(100);
 
   send(
     data: any,
@@ -31,8 +97,26 @@ export class ShinyComm {
       // this doesn't seem relevant to the widget?
       header: {}
     };
+
     const msg_txt = JSON.stringify(msg);
-    Shiny.setInputValue("shinywidgets_comm_send", msg_txt, {priority: "event"});
+
+    // Ex `data` value:
+    // {"method": "custom","content": {    "event": "interaction",    "type": "mousemove",    "coordinates": [        -17.76259815404015,        12.096729340756617    ]}}
+    if (
+      data.method === "custom" &&
+      data.content.event === "interaction" &&
+      data.content.type === "mousemove"
+    ) {
+      // mousemove events only
+      this._mouse_move_throttler.throttle(() => {
+        Shiny.setInputValue("shinywidgets_comm_send", msg_txt, {priority: "event"});
+      })
+    } else {
+      // Mouse over, mouse out, click, etc. (Anything other than `"mousemove"`)
+      this._mouse_move_throttler.flush()
+      Shiny.setInputValue("shinywidgets_comm_send", msg_txt, {priority: "event"});
+    }
+
 
     // When client-side changes happen to the WidgetModel, this send method
     // won't get called for _every_  change (just the first one). The
@@ -42,7 +126,8 @@ export class ShinyComm {
     // https://github.com/jupyter-widgets/ipywidgets/blob/88cec8b/packages/base/src/widget.ts#L550-L557
     if (callbacks && callbacks.iopub && callbacks.iopub.status) {
       setTimeout(() => {
-        // TODO: Call this when Shiny reports that it is idle?
+        // TODO-future: this doesn't seem quite right. Maybe listen to the shiny-busy flag?
+        // const state = document.querySelector("html").classList.contains("shiny-busy") ? "busy" : "idle";
         const msg = {content: {execution_state: "idle"}};
         callbacks.iopub.status(msg);
       }, 0);
