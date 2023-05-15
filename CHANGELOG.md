1acbef973da065c8db5716ee167dbc2a8c0bab2d
Carson <cpsievert1@gmail.com>
2023-05-15 10:33:45 -0500

    Actually export as_widget()

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,11 @@ All notable changes to shinywidgets will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [0.2.0]
+## [0.2.1]
+
+* Actually export `as_widget()` (it was mistakenly not exported in 0.2.0).
+
+## [0.2.0] - 2023-04-13
 
 * Closed #43: Fixed an issue where widgets would sometimes not load in a dynamic UI context. (#91, #93)
 * Closed #14: Added a `bokeh_dependency()` function to simplify use of bokeh widgets. (#85)
--- shinywidgets/__init__.py
+++ shinywidgets/__init__.py
@@ -2,9 +2,9 @@
 
 __author__ = """Carson Sievert"""
 __email__ = "carson@rstudio.com"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 from ._dependencies import bokeh_dependency
-from ._shinywidgets import output_widget, reactive_read, register_widget, render_widget
+from ._shinywidgets import output_widget, reactive_read, register_widget, render_widget, as_widget
 
-__all__ = ("output_widget", "register_widget", "render_widget", "reactive_read", "bokeh_dependency")
+__all__ = ("output_widget", "register_widget", "render_widget", "reactive_read", "bokeh_dependency", "as_widget")
