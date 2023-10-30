ba24618de9d955384108577aecef7fd4bfbd3f6b
Carson <cpsievert1@gmail.com>
2023-10-30 16:27:41 -0500

    Start v0.2.2 release candidate

--- shinywidgets/__init__.py
+++ shinywidgets/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Carson Sievert"""
 __email__ = "carson@rstudio.com"
-__version__ = "0.2.1.9000"
+__version__ = "0.2.2"
 
 from ._dependencies import bokeh_dependency
 from ._shinywidgets import output_widget, reactive_read, register_widget, render_widget, as_widget
