ce07ff54bff6f6dcb6ff6526aade401ef15afd9e
Carson <cpsievert1@gmail.com>
2023-11-07 13:41:59 -0600

    Bump version; update changelog

--- shinywidgets/__init__.py
+++ shinywidgets/__init__.py
@@ -2,9 +2,15 @@
 
 __author__ = """Carson Sievert"""
 __email__ = "carson@rstudio.com"
-__version__ = "0.2.2"
+__version__ = "0.2.2.9000"
 
 from ._dependencies import bokeh_dependency
-from ._shinywidgets import output_widget, reactive_read, register_widget, render_widget, as_widget
+from ._shinywidgets import (
+    as_widget,
+    output_widget,
+    reactive_read,
+    register_widget,
+    render_widget,
+)
 
 __all__ = ("output_widget", "register_widget", "render_widget", "reactive_read", "bokeh_dependency", "as_widget")
