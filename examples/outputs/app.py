11990def6778b7bdbb8da943e6a68601d4336b46
Carson <cpsievert1@gmail.com>
2023-11-13 10:20:16 -0600

    Make example code more readable/minimal

--- examples/outputs/app.py
+++ examples/outputs/app.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 from shiny import *
 
 from shinywidgets import *
@@ -40,68 +39,6 @@ def server(input: Inputs, output: Outputs, session: Session):
             full_screen=True,
         )
 
-    @output(id="ipyleaflet")
-    @render_widget
-    def _():
-        from ipyleaflet import Map, Marker
-
-        m = Map(center=(52.204793, 360.121558), zoom=4)
-        m.add_layer(Marker(location=(52.204793, 360.121558)))
-        return m
-
-    @output(id="qgrid")
-    @render_widget
-    def _():
-        import qgrid
-
-        randn = np.random.randn
-        df_types = pd.DataFrame(
-            {
-                "A": pd.Series(
-                    [
-                        "2013-01-01",
-                        "2013-01-02",
-                        "2013-01-03",
-                        "2013-01-04",
-                        "2013-01-05",
-                        "2013-01-06",
-                        "2013-01-07",
-                        "2013-01-08",
-                        "2013-01-09",
-                    ],
-                    index=list(range(9)),
-                    dtype="datetime64[ns]",
-                ),
-                "B": pd.Series(randn(9), index=list(range(9)), dtype="float32"),
-                "C": pd.Categorical(
-                    [
-                        "washington",
-                        "adams",
-                        "washington",
-                        "madison",
-                        "lincoln",
-                        "jefferson",
-                        "hamilton",
-                        "roosevelt",
-                        "kennedy",
-                    ]
-                ),
-                "D": [
-                    "foo",
-                    "bar",
-                    "buzz",
-                    "bippity",
-                    "boppity",
-                    "foo",
-                    "foo",
-                    "bar",
-                    "zoo",
-                ],
-            }
-        )
-        df_types["E"] = df_types["D"] == "foo"
-        return qgrid.show_grid(df_types, show_toolbar=True)
-
     @output(id="altair")
     @render_widget
     def _():
@@ -128,6 +65,68 @@ def server(input: Inputs, output: Outputs, session: Session):
             y=np.random.randn(100),
             color=np.random.randn(100),
         )
+
+    @output(id="ipyleaflet")
+    @render_widget
+    def _():
+        from ipyleaflet import Map, Marker
+
+        m = Map(center=(52.204793, 360.121558), zoom=4)
+        m.add_layer(Marker(location=(52.204793, 360.121558)))
+        return m
+
+    @output(id="pydeck")
+    @render_widget
+    def _():
+        import pydeck as pdk
+
+        UK_ACCIDENTS_DATA = "https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv"
+
+        layer = pdk.Layer(
+            "HexagonLayer",  # `type` positional argument is here
+            UK_ACCIDENTS_DATA,
+            get_position=["lng", "lat"],
+            auto_highlight=True,
+            elevation_scale=50,
+            pickable=True,
+            elevation_range=[0, 3000],
+            extruded=True,
+            coverage=1,
+        )
+
+        # Set the viewport location
+        view_state = pdk.ViewState(
+            longitude=-1.415,
+            latitude=52.2323,
+            zoom=6,
+            min_zoom=5,
+            max_zoom=15,
+            pitch=40.5,
+            bearing=-27.36,
+        )
+
+        # Combined all of it and render a viewport
+        return pdk.Deck(layers=[layer], initial_view_state=view_state)
+
+    @output(id="ipysigma")
+    @render_widget
+    def _():
+        import igraph as ig
+        from ipysigma import Sigma
+        g = ig.Graph.Famous('Zachary')
+        return Sigma(g, node_size=g.degree, node_color=g.betweenness(), node_color_gradient='Viridis')
+
+
+    @output(id="bokeh")
+    @render_widget
+    def _():
+        from bokeh.plotting import figure
+
+        x = [1, 2, 3, 4, 5]
+        y = [6, 7, 2, 4, 5]
+        p = figure(title="Simple line example", x_axis_label="x", y_axis_label="y")
+        p.line(x, y, legend_label="Temp.", line_width=2)
+        return p
 
     @output(id="bqplot")
     @render_widget
@@ -203,66 +202,6 @@ def server(input: Inputs, output: Outputs, session: Session):
                 "video": {"width": 640, "height": 480},
             }
         )
-
-    @output(id="ipyvolume")
-    @render_widget
-    def _():
-        from ipyvolume import quickquiver
-
-        x, y, z, u, v, w = np.random.random((6, 1000)) * 2 - 1
-        return quickquiver(x, y, z, u, v, w, size=5)
-
-    @output(id="ipysigma")
-    @render_widget
-    def _():
-        import igraph as ig
-        from ipysigma import Sigma
-        g = ig.Graph.Famous('Zachary')
-        return Sigma(g, node_size=g.degree, node_color=g.betweenness(), node_color_gradient='Viridis')
-
-    @output(id="pydeck")
-    @render_widget
-    def _():
-        import pydeck as pdk
-
-        UK_ACCIDENTS_DATA = "https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv"
-
-        layer = pdk.Layer(
-            "HexagonLayer",  # `type` positional argument is here
-            UK_ACCIDENTS_DATA,
-            get_position=["lng", "lat"],
-            auto_highlight=True,
-            elevation_scale=50,
-            pickable=True,
-            elevation_range=[0, 3000],
-            extruded=True,
-            coverage=1,
-        )
-
-        # Set the viewport location
-        view_state = pdk.ViewState(
-            longitude=-1.415,
-            latitude=52.2323,
-            zoom=6,
-            min_zoom=5,
-            max_zoom=15,
-            pitch=40.5,
-            bearing=-27.36,
-        )
-
-        # Combined all of it and render a viewport
-        return pdk.Deck(layers=[layer], initial_view_state=view_state)
-
-    @output(id="bokeh")
-    @render_widget
-    def _():
-        from bokeh.plotting import figure
-
-        x = [1, 2, 3, 4, 5]
-        y = [6, 7, 2, 4, 5]
-        p = figure(title="Simple line example", x_axis_label="x", y_axis_label="y")
-        p.line(x, y, legend_label="Temp.", line_width=2)
-        return p
 
 
 app = App(app_ui, server)
