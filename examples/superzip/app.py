a0eabbbe10b3aa7471e1300e331c3bffa260be7f
Carson <cpsievert1@gmail.com>
2024-04-09 12:11:42 -0500

    Improve superzip example

--- examples/superzip/app.py
+++ examples/superzip/app.py
@@ -1,39 +1,17 @@
-import os
-from typing import List, Optional, Tuple
+from pathlib import Path
 
 import ipyleaflet as leaf
 import ipywidgets
-import matplotlib as mpl
-import numpy as np
 import pandas as pd
-import plotly.figure_factory as ff
-import plotly.graph_objs as go
-from htmltools import head_content
-from ipyleaflet import basemaps
-from matplotlib import cm
-from shiny import *
-from shiny.types import SilentException
+from faicons import icon_svg
+from shiny import App, Inputs, Outputs, Session, reactive, render, req, ui
+from utils import col_numeric, create_map, density_plot, heatmap_gradient
 
-from shinywidgets import *
+from shinywidgets import output_widget, reactive_read, render_plotly, render_widget
 
-color_palette = cm.get_cmap("viridis", 10)
-
-
-# TODO: how to handle nas (pd.isna)?
-def col_numeric(domain: Tuple[float, float], na_color: str = "#808080"):
-    rescale = mpl.colors.Normalize(domain[0], domain[1])
-
-    def _(vals: List[float]) -> List[str]:
-        cols = color_palette(rescale(vals))
-        return [mpl.colors.to_hex(v) for v in cols]
-
-    return _
-
-
-# TODO: when this issue is fixed, we won't have to sample anymore
-# https://github.com/rstudio/prism/issues/119
-app_dir = os.path.dirname(__file__)
-allzips = pd.read_csv(os.path.join(app_dir, "superzip.csv")).sample(
+# Load data
+app_dir = Path(__file__).parent
+allzips = pd.read_csv(app_dir / "superzip.csv").sample(
     n=10000, random_state=42
 )
 
@@ -42,137 +20,70 @@ allzips = pd.read_csv(os.path.join(app_dir, "superzip.csv")).sample(
 # ------------------------------------------------------------------------
 
 vars = {
-    "Score": "Overall score",
+    "Score": "overall superzip score",
     "College": "% college educated",
-    "Income": "Median income",
-    "Population": "Population",
+    "Income": "median income",
+    "Population": "population",
 }
 
-css = open(os.path.join(app_dir, "styles.css"), "r").readlines()
-
-ui_map = ui.TagList(
-    output_widget("map", width="100%", height="100%"),
-    ui.panel_fixed(
-        ui.h2("SuperZIP explorer"),
-        ui.input_select("variable", "Heatmap variable", vars),
-        output_widget("density_score", height="200px"),
-        output_widget("density_college", height="200px"),
-        output_widget("density_income", height="200px"),
-        output_widget("density_pop", height="200px"),
-        id="controls",
-        class_="panel panel-default",
-        width="330px",
-        height="auto",
-        draggable=True,
-        top="60px",
-        left="auto",
-        right="20px",
-        bottom="auto",
-    ),
-    ui.div(
-        "Data compiled for ",
-        ui.tags.em("Coming Apart: The State of White America, 1960-2010"),
-        " by Charles Murray (Crown Forum, 2012).",
-        id="cite",
-    ),
-)
-
 app_ui = ui.page_navbar(
-    ui.nav(
+    ui.nav_spacer(),
+    ui.nav_panel(
         "Interactive map",
-        ui.div(head_content(ui.tags.style(css)), ui_map, class_="outer"),
-    ),
-    ui.nav(
-        "Data explorer",
-        ui.row(
-            ui.column(3, ui.output_ui("data_intro")),
-            ui.column(9, output_widget("data", height="100%")),
-        ),
-        ui.row(
-            ui.column(2),
-            ui.column(8, output_widget("table_map")),
-            ui.column(2),
-        ),
-    ),
-    title="Superzip",
-)
-
-# ------------------------------------------------------------------------
-# non-reactive helper functions
-# ------------------------------------------------------------------------
-
-
-def density_plot(
-    overall: pd.DataFrame,
-    in_bounds: pd.DataFrame,
-    var: str,
-    selected: Optional[pd.DataFrame] = None,
-    title: Optional[str] = None,
-    showlegend: bool = False,
-):
-    dat = [overall[var], in_bounds[var]]
-    if var == "Population":
-        dat = [np.log10(x) for x in dat]
-
-    # Create distplot with curve_type set to 'normal'
-    fig = ff.create_distplot(
-        dat,
-        ["Overall", "In bounds"],
-        colors=["black", "#6DCD59"],
-        show_rug=False,
-        show_hist=False,
-    )
-    # Remove tick labels
-    fig.update_layout(
-        # hovermode="x",
-        height=200,
-        showlegend=showlegend,
-        margin=dict(l=0, r=0, t=0, b=0),
-        legend=dict(x=0.5, y=1, orientation="h", xanchor="center", yanchor="bottom"),
-        xaxis=dict(
-            title=title if title is not None else var,
-            showgrid=False,
-            showline=False,
-            zeroline=False,
-        ),
-        yaxis=dict(
-            showgrid=False,
-            showline=False,
-            showticklabels=False,
-            zeroline=False,
-        ),
-    )
-    # hovermode itsn't working properly when dynamically, absolutely positioned
-    for _, trace in enumerate(fig.data):
-        trace.update(hoverinfo="none")
-
-    if selected is not None:
-        x = selected[var].tolist()[0]
-        if var == "Population":
-            x = np.log10(x)
-        fig.add_shape(
-            type="line",
-            x0=x,
-            x1=x,
-            y0=0,
-            y1=1,
-            yref="paper",
-            line=dict(width=1, dash="dashdot", color="gray"),
+        ui.layout_sidebar(
+            ui.sidebar(
+                output_widget("density_score"),
+                output_widget("density_college"),
+                output_widget("density_income"),
+                output_widget("density_pop"),
+                position="right",
+                width=300,
+                title=ui.tooltip(
+                    ui.span(
+                        "Overall density vs in bounds",
+                        icon_svg("circle-info").add_class("ms-2"),
+                    ),
+                    "The density plots show how various metrics behave overall (black) in comparison to within view (green)."
+                )
+            ),
+            ui.card(
+                ui.card_header(
+                    "Heatmap showing ",
+                    ui.input_select("variable", None, vars, width="auto"),
+                    class_="d-flex align-items-center gap-3"
+                ),
+                output_widget("map"),
+                ui.card_footer(
+                    ui.markdown("Data compiled for _Coming Apart: The State of White America, 1960-2010_ by Charles Murray (Crown Forum, 2012).")
+                ),
+                full_screen=True
+            )
         )
-
-    return go.FigureWidget(data=fig.data, layout=fig.layout)
-
-
-def create_map(**kwargs):
-    map = leaf.Map(
-        center=(37.45, -88.85),
-        zoom=4,
-        scroll_wheel_zoom=True,
-        attribution_control=False,
-        **kwargs,
+    ),
+    ui.nav_panel(
+        "Data explorer",
+        ui.layout_columns(
+            ui.output_ui("data_intro"),
+            ui.output_data_frame("zips_data"),
+            col_widths=[3, 9]
+        ),
+        ui.layout_columns(
+            output_widget("table_map"),
+            col_widths=[-2, 8, -2]
+        )
+    ),
+    fillable="Interactive map",
+    id="navbar",
+    header=ui.include_css(app_dir / "styles.css"),
+    title=ui.popover(
+        [
+            "Superzip explorer",
+            icon_svg("circle-info").add_class("ms-2"),
+        ],
+        ui.markdown("'Super Zips' is a term [coined by Charles Murray](https://www.amazon.com/Coming-Apart-State-America-1960-2010/dp/030745343X) to describe the most prosperous, highly educated demographic clusters"),
+        placement="right",
     )
-    map.add_layer(leaf.basemap_to_tiles(basemaps.CartoDB.DarkMatter))
-    return map
+)
 
 
 # ------------------------------------------------------------------------
@@ -184,35 +95,37 @@ def server(input: Inputs, output: Outputs, session: Session):
     # ------------------------------------------------------------------------
     # Main map logic
     # ------------------------------------------------------------------------
-    map = create_map(layout=ipywidgets.Layout(width="100%", height="100%"))
-    register_widget("map", map)
+    @render_widget
+    def map():
+        return create_map()
 
     # Keeps track of whether we're showing markers (zoomed in) or heatmap (zoomed out)
-    show_markers = reactive.Value(False)
+    show_markers = reactive.value(False)
 
-    @reactive.Effect
+    # Switch from heatmap to markers when zoomed into 200 or fewer zips
+    @reactive.effect
     def _():
         nzips = zips_in_bounds().shape[0]
         show_markers.set(nzips < 200)
 
     # When the variable changes, either update marker colors or redraw the heatmap
-    @reactive.Effect
+    @reactive.effect
     @reactive.event(input.variable)
     def _():
         zips = zips_in_bounds()
         if not show_markers():
             remove_heatmap()
-            map.add_layer(layer_heatmap())
+            map.widget.add_layer(layer_heatmap())
         else:
             zip_colors = dict(zip(zips.Zipcode, zips_marker_color()))
-            for x in map.layers:
+            for x in map.widget.layers:
                 if x.name.startswith("marker-"):
                     zipcode = int(x.name.split("-")[1])
                     if zipcode in zip_colors:
                         x.color = zip_colors[zipcode]
 
     # When bounds change, maybe add new markers
-    @reactive.Effect
+    @reactive.effect
     @reactive.event(lambda: zips_in_bounds())
     def _():
         if not show_markers():
@@ -223,37 +136,45 @@ def server(input: Inputs, output: Outputs, session: Session):
 
         # Be careful not to create markers until we know we need to add it
         current_markers = set(
-            [m.name for m in map.layers if m.name.startswith("marker-")]
+            [m.name for m in map.widget.layers if m.name.startswith("marker-")]
         )
         zips["Color"] = zips_marker_color()
         for _, row in zips.iterrows():
             if ("marker-" + str(row.Zipcode)) not in current_markers:
-                map.add_layer(create_marker(row, color=row.Color))
+                map.widget.add_layer(create_marker(row, color=row.Color))
 
     # Change from heatmap to markers: remove the heatmap and show markers
     # Change from markers to heatmap: hide the markers and add the heatmap
-    @reactive.Effect
+    @reactive.effect
     @reactive.event(show_markers)
     def _():
         if show_markers():
-            map.remove_layer(layer_heatmap())
+            map.widget.remove_layer(layer_heatmap())
         else:
-            map.add_layer(layer_heatmap())
+            map.widget.add_layer(layer_heatmap())
 
         opacity = 0.6 if show_markers() else 0.0
 
-        for x in map.layers:
+        for x in map.widget.layers:
             if x.name.startswith("marker-"):
                 x.fill_opacity = opacity
                 x.opacity = opacity
 
-    @reactive.Calc
+    # For some reason, ipyleaflet updates bounds to an incorrect value
+    # when the map is hidden, so only update the bounds when the map is visible
+    current_bounds = reactive.value()
+
+    @reactive.effect
+    def _():
+        bb = reactive_read(map.widget, "bounds")
+        if input.navbar() != "Interactive map":
+            return
+        with reactive.isolate():
+            current_bounds.set(bb)
+
+    @reactive.calc
     def zips_in_bounds():
-        bb = reactive_read(map, "bounds")
-        if not bb:
-            # TODO: this should really be `raise SilentException`...why doesn't it work?
-            # return pd.DataFrame()
-            raise SilentException
+        bb = req(current_bounds())
 
         lats = (bb[0][0], bb[1][0])
         lons = (bb[0][1], bb[1][1])
@@ -264,44 +185,31 @@ def server(input: Inputs, output: Outputs, session: Session):
             & (allzips.Long <= lons[1])
         ]
 
-    @reactive.Calc
+    @reactive.calc
     def zips_marker_color():
         vals = allzips[input.variable()]
         domain = (vals.min(), vals.max())
         vals_in_bb = zips_in_bounds()[input.variable()]
         return col_numeric(domain)(vals_in_bb)
 
-    @reactive.Calc
+    @reactive.calc
     def layer_heatmap():
         locs = allzips[["Lat", "Long", input.variable()]].to_numpy()
         return leaf.Heatmap(
             locations=locs.tolist(),
             name="heatmap",
-            # R> cat(paste0(round(scales::rescale(log10(1:10), to = c(0.05, 1)), 2), ": '", viridis::viridis(10), "'"), sep = "\n")
-            gradient={
-                0.05: "#440154",
-                0.34: "#482878",
-                0.5: "#3E4A89",
-                0.62: "#31688E",
-                0.71: "#26828E",
-                0.79: "#1F9E89",
-                0.85: "#35B779",
-                0.91: "#6DCD59",
-                0.96: "#B4DE2C",
-                1: "#FDE725",
-            },
+            gradient=heatmap_gradient,
         )
 
     def remove_heatmap():
-        for x in map.layers:
+        for x in map.widget.layers:
             if x.name == "heatmap":
-                map.remove_layer(x)
+                map.widget.remove_layer(x)
 
-    zip_selected = reactive.Value(None)
+    zip_selected = reactive.value(None)
 
-    @output(id="density_score")
-    @render_widget
-    def _():
+    @render_plotly
+    def density_score():
         return density_plot(
             allzips,
             zips_in_bounds(),
@@ -311,23 +219,26 @@ def server(input: Inputs, output: Outputs, session: Session):
             showlegend=True,
         )
 
-    @output(id="density_income")
-    @render_widget
-    def _():
+    @render_plotly
+    def density_income():
         return density_plot(
-            allzips, zips_in_bounds(), selected=zip_selected(), var="Income"
+            allzips,
+            zips_in_bounds(),
+            selected=zip_selected(),
+            var="Income"
         )
 
-    @output(id="density_college")
-    @render_widget
-    def _():
+    @render_plotly
+    def density_college():
         return density_plot(
-            allzips, zips_in_bounds(), selected=zip_selected(), var="College"
+            allzips,
+            zips_in_bounds(),
+            selected=zip_selected(),
+            var="College"
         )
 
-    @output(id="density_pop")
-    @render_widget
-    def _():
+    @render_plotly
+    def density_pop():
         return density_plot(
             allzips,
             zips_in_bounds(),
@@ -336,9 +247,61 @@ def server(input: Inputs, output: Outputs, session: Session):
             title="log10(Population)",
         )
 
+    @render.ui
+    def data_intro():
+        zips = zips_in_bounds()
+
+        md = ui.markdown(
+            f"""
+            {zips.shape[0]} zip codes are currently within the map's viewport, and amongst them:
+
+              * {100*zips.Superzip.mean():.1f}% are superzips
+              * Mean income is ${zips.Income.mean():.0f}k  💰
+              * Mean population is {zips.Population.mean():.0f} 👨🏽👩🏽👦🏽
+              * Mean college educated is {zips.College.mean():.1f}% 🎓
+
+            Use the filter controls on the table's columns to drill down further or
+            click on a row to
+            """,
+        )
+
+        return ui.div(md, class_="my-3 lead")
+
+    @render.data_frame
+    def zips_data():
+        return render.DataGrid(zips_in_bounds(), selection_mode="row")
+
+    @reactive.calc
+    def selected_row():
+        sel = zips_data.input_cell_selection()
+        print(sel)
+        if not sel["rows"]:
+            return pd.DataFrame()
+        return zips_data.data().iloc[sel["rows"][0]]
+
+    @render_widget
+    def table_map():
+        #req(not selected_row().empty)
+
+        return create_map()
+
+    @reactive.effect
+    @reactive.event(selected_row)
+    def _():
+        if selected_row().empty:
+            return
+        for x in table_map.widget.layers:
+            if x.name.startswith("marker"):
+                table_map.widget.remove_layer(x)
+        m = create_marker(selected_row())
+        table_map.widget.add_layer(m)
+
+    # Utility function to create a marker
     def create_marker(row, **kwargs):
         m = leaf.CircleMarker(
             location=(row.Lat, row.Long),
+            # Currently doesn't work with ipywidgets >8.0
+            # https://github.com/posit-dev/py-shinywidgets/issues/101
             popup=ipywidgets.HTML(
                 f"""
             {row.City}, {row.State} ({row.Zipcode})<br/>
@@ -360,72 +323,6 @@ def server(input: Inputs, output: Outputs, session: Session):
         m.on_click(_on_click)
 
         return m
-
-    @output(id="data_intro")
-    @render.ui
-    def _():
-        zips = zips_in_bounds()
-
-        md = ui.markdown(
-            f"""
-            {zips.shape[0]} zip codes are currently within the map's viewport, and amongst them:
-
-              * {100*zips.Superzip.mean():.1f}% are superzips
-              * Mean income is ${zips.Income.mean():.0f}k  💰
-              * Mean population is {zips.Population.mean():.0f} 👨🏽👩🏽👦🏽
-              * Mean college educated is {zips.College.mean():.1f}% 🎓
-
-            Use the filter controls on the table's columns to drill down further or
-            click on a row to
-            """,
-        )
-
-        return ui.div(md, class_="my-3 lead")
-
-    selected_table_row = reactive.Value(pd.DataFrame())
-
-    @output(id="data")
-    @render_widget
-    def _():
-        import qgrid
-
-        dat = zips_in_bounds().drop(["Lat", "Long", "Color"], axis=1, errors="ignore")
-
-        w = qgrid.show_grid(
-            dat,
-            grid_options={"editable": False},
-            column_definitions={"index": {"maxWidth": 0, "minWidth": 0, "width": 0}},
-        )
-
-        def _on_change(event, widget):
-            idx = event["new"][0]
-            selected_table_row.set(zips_in_bounds().iloc[[idx]])
-
-        w.on("selection_changed", _on_change)
-
-        return w
-
-    table_map = create_map()
-
-    @output(id="table_map")
-    @render_widget
-    def _():
-        if selected_table_row().empty:
-            return None
-        else:
-            return table_map
-
-    # TODO: currently there is a bug where clicking the popup causes an error,
-    # but I _think_ this'll get fixed in the next release of ipywidgets/ipyleaflet
-    # https://github.com/jupyter-widgets/ipywidgets/issues/3384
-    @reactive.Effect
-    @reactive.event(selected_table_row)
-    def _():
-        for x in table_map.layers:
-            if x.name.startswith("marker"):
-                table_map.remove_layer(x)
-        for _, row in selected_table_row().iterrows():
-            table_map.add_layer(create_marker(row))
 
 
 app = App(app_ui, server)
---
+++ examples/superzip/utils.py
@@ -0,0 +1,119 @@
+from typing import List, Optional, Tuple
+
+import ipyleaflet as leaf
+import matplotlib as mpl
+import numpy as np
+import pandas as pd
+import plotly.figure_factory as ff
+import plotly.graph_objs as go
+import shiny
+from ipyleaflet import basemaps
+from matplotlib import cm
+
+
+def create_map(**kwargs):
+    map = leaf.Map(
+        center=(37.45, -88.85),
+        zoom=4,
+        scroll_wheel_zoom=True,
+        attribution_control=False,
+        **kwargs,
+    )
+    map.add_layer(leaf.basemap_to_tiles(basemaps.CartoDB.DarkMatter))
+    search = leaf.SearchControl(
+        url="https://nominatim.openstreetmap.org/search?format=json&q={s}",
+        position="topleft",
+        zoom=10,
+    )
+    map.add(search)
+    return map
+
+def density_plot(
+    overall: pd.DataFrame,
+    in_bounds: pd.DataFrame,
+    var: str,
+    selected: Optional[pd.DataFrame] = None,
+    title: Optional[str] = None,
+    showlegend: bool = False,
+):
+    shiny.req(not in_bounds.empty)
+
+    dat = [overall[var], in_bounds[var]]
+    if var == "Population":
+        dat = [np.log10(x) for x in dat]
+
+    # Create distplot with curve_type set to 'normal'
+    fig = ff.create_distplot(
+        dat,
+        ["Overall", "In bounds"],
+        colors=["black", "#6DCD59"],
+        show_rug=False,
+        show_hist=False,
+    )
+    # Remove tick labels
+    fig.update_layout(
+        # hovermode="x",
+        height=200,
+        showlegend=showlegend,
+        margin=dict(l=0, r=0, t=0, b=0),
+        legend=dict(x=0.5, y=1, orientation="h", xanchor="center", yanchor="bottom"),
+        xaxis=dict(
+            title=title if title is not None else var,
+            showline=False,
+            zeroline=False,
+        ),
+        yaxis=dict(
+            showgrid=False,
+            showline=False,
+            showticklabels=False,
+            zeroline=False,
+        ),
+        template="plotly_white"
+    )
+    # hovermode itsn't working properly when dynamically, absolutely positioned
+    for _, trace in enumerate(fig.data):
+        trace.update(hoverinfo="none")
+
+    if selected is not None:
+        x = selected[var].tolist()[0]
+        if var == "Population":
+            x = np.log10(x)
+        fig.add_shape(
+            type="line",
+            x0=x,
+            x1=x,
+            y0=0,
+            y1=1,
+            yref="paper",
+            line=dict(width=1, dash="dashdot", color="gray"),
+        )
+
+    return go.FigureWidget(data=fig.data, layout=fig.layout)
+
+
+color_palette = cm.get_cmap("viridis", 10)
+
+# TODO: how to handle nas (pd.isna)?
+def col_numeric(domain: Tuple[float, float], na_color: str = "#808080"):
+    rescale = mpl.colors.Normalize(domain[0], domain[1])
+
+    def _(vals: List[float]) -> List[str]:
+        cols = color_palette(rescale(vals))
+        return [mpl.colors.to_hex(v) for v in cols]
+
+    return _
+
+
+# R> cat(paste0(round(scales::rescale(log10(1:10), to = c(0.05, 1)), 2), ": '", viridis::viridis(10), "'"), sep = "\n")
+heatmap_gradient = {
+    0.05: "#440154",
+    0.34: "#482878",
+    0.5: "#3E4A89",
+    0.62: "#31688E",
+    0.71: "#26828E",
+    0.79: "#1F9E89",
+    0.85: "#35B779",
+    0.91: "#6DCD59",
+    0.96: "#B4DE2C",
+    1: "#FDE725",
+}
